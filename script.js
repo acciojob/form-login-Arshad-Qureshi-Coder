12dc77184f78be34cc7889e655a338d3dbd1bec2
Arshad_Qureshi <130859896+Arshad-Qureshi-Coder@users.noreply.github.com>
2023-06-19 23:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+ var firstName = document.getElementsByName('fname')[0].value;
+  var lastName = document.getElementsByName('lname')[0].value;
 
+  // Display the first and last name using alert
+  alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
 }
